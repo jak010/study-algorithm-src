3b9ce6a2a92d63b462126b265b87be78e35a2edb
bluetoonist <bluetoon@naver.com>
2020-07-14 19:31:39 +0900

    [U] BackJoon Pushed

---
+++ BackJoon/10171.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    printf("\\    /\\\n");
+    printf(" )  ( ')\n");
+    printf("(  /  )\n");
+    printf(" \\(__)|\n");
+
+
+    printf("\\    /\\\n");
+    printf(" )  ( ')\n");
+    printf("(  /  )\n");
+    printf(" \\(__)|\n");
+
+    return 0;
+} 
