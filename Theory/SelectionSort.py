db5177bcc26acee54d8c0ebcf3d74fb7d9df4410
jak010 <bluetoon@naver.com>
2021-06-04 00:54:21 +0900

    선택 정렬 추가

---
+++ Theory/SelectionSort.py
@@ -0,0 +1,17 @@
+arr = [1, 2, 3, 4, 5]
+
+for idx, value in enumerate(arr):
+    # 최소값은 시작 인덱스로 초기화
+    current_idx = idx
+
+    # 선택정렬 조건: 최소값이 있어야함 (오름차순)
+    for _id in range(0, len(arr)):
+        if arr[_id] < arr[current_idx]:
+            arr[_id], arr[idx] = arr[idx], arr[_id]
+
+    # 선택정렬 (내림차순)
+    # for _id in range(0, len(arr)):
+    #     if arr[_id] > arr[current_idx]:
+    #         arr[_id], arr[idx] = arr[idx], arr[_id]
+
+print(arr)
