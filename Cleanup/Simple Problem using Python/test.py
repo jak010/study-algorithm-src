036135d4f0971740f5312225529031fc1f639b2e
jak010 <bluetoon@naver.com>
2021-10-10 12:45:54 +0900

    CleanUp

--- Cleanup/Simple Problem using Python/test.py
+++ Cleanup/Simple Problem using Python/test.py
@@ -1,23 +1,22 @@
 num = int(input("input Number:"))
-d=1
+d = 1
 n_sum = []
 n2_sum = []
 while d <= num:
-    if  num % d == 0:
+    if num % d == 0:
         n_sum.append(d)
-        d = d+1
+        d = d + 1
     else:
-        d=d+1
+        d = d + 1
 
 print(n_sum)
 
 for i in n_sum:
-    if ((n_sum[0]*n_sum[1+(i-1)])==n_sum[-1]):
+    if ((n_sum[0] * n_sum[1 + (i - 1)]) == n_sum[-1]):
         n2_sum.append(i)
-    
-    
+
+
     else:
         pass
 
 print(n2_sum)
-
--- Notepad/Notepad_fibonacci.py
+++
@@ -1,9 +0,0 @@
-def find_fibonacchi_seq_iter(n):
-    if n < 2: pass
-    a, b = 0, 1
-    for i in range(n):
-        a, b = b, a + b
-    return a
-
-
-print(find_fibonacchi_seq_iter(10))
