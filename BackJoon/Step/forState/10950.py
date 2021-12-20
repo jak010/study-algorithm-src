25c3f994ef4b17ad1d68513a00d4251732e5e1a9
jak010 <bluetoon@naver.com>
2021-12-20 23:59:23 +0900

    feat: boj append

---
+++ BackJoon/Step/forState/10950.py
@@ -0,0 +1,5 @@
+test_count = int(input())
+
+for x in range(test_count):
+    number1, number2 = map(int, input().split())
+    print(number1 + number2)
---
+++ BackJoon/Step/forState/2739.py
@@ -0,0 +1,4 @@
+number = int(input())
+
+for _index in range(1, 10):
+    print(number, "*", str(_index), "=", number * _index)
