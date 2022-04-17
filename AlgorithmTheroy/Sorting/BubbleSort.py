27b0cbc8d956e2f911f53c372c24484711857a24
jako <bluetoon@naver.com>
2022-04-18 00:02:40 +0900

    update bubble sort

--- AlgorithmTheroy/Sorting/BubbleSort.py
+++ AlgorithmTheroy/Sorting/BubbleSort.py
@@ -10,11 +10,19 @@ Description:
 
 """
 
-arr = [1, 3, 2, 5, 4]
+arr = [5, 3, 7, 9, 1]
 
-for idx in range(len(arr)):
-    for j in range(len(arr) - 1):
-        if arr[j] < arr[j + 1]:
+for i in range(len(arr)):  # 반복 횟수
+    for j in range((len(arr) - 1) - i):  # 탐색 범위를 배열의 끝에서부터 -1씩 좁아짐
+        if arr[j] > arr[j + 1]:
+            print(arr)
             arr[j], arr[j + 1] = arr[j + 1], arr[j]
+            print(arr)
 
-print(arr)
+
+# for idx in range(len(arr)):
+#     for j in range(len(arr) - 1):
+#         if arr[j] < arr[j + 1]:
+#             arr[j], arr[j + 1] = arr[j + 1], arr[j]
+#
+# print(arr)
