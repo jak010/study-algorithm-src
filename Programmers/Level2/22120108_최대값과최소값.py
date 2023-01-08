fd576940ddfd6ec908d3c16f9811bc09cd3bf2f7
jak010 <bluetoon@naver.com>
2023-01-08 14:04:22 +0900

    <feat> 프로그래머스 최소값과 최대값

--- Programmers/Level2/22120108_최대값과최소값.py
+++ Programmers/Level2/22120108_최대값과최소값.py
@@ -1,3 +1,6 @@
+from typing import Tuple, List
+
+
 def solution(s):
     split_of_space = [int(x) for x in s.split(" ")]
 
@@ -11,8 +14,36 @@ def solution2(s):
     return f"{s[0]} {s[-1]}"
 
 
+def solution3(s):
+    space_index = []
+    str_length = len(s)
+
+    for index, ch in enumerate(s):
+        if ch.isspace():
+            space_index.append(index)
+
+    space_index.insert(0, 0)
+    space_index.append(str_length)
+
+    offset_range: List[Tuple[int, int]] = []
+    for index in range(len(space_index)):
+        if index == 0:
+            continue
+
+        offset_range.append((space_index[index - 1], space_index[index]))
+
+    answer_result = []
+    for offset in offset_range:
+        result = int(s[offset[0]: offset[1]].strip())
+        answer_result.append(result)
+
+    return f"{min(answer_result)} {max(answer_result)}"
+
+
 if __name__ == '__main__':
-    strs = "1 2 3 4"
-    s = solution2(strs)
+    # strs = "1 2 3 4"
+    strs = "-1 -2 -3 -4"
+    # print(len(strs))
+    s = solution3(strs)
 
     print(s)
